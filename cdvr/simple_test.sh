df40981f7a4e138fee7ff05f1635a9fccc08b7b2
diegovr <ciegovr@gmail.com>
2014-04-24 15:46:34 -0500

    first commit

---
+++ cdvr/simple_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python mapper.py < testdata/breastcancerwisconsin.data
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Crawler',
+      version='0.01',
+      description='Algorithms for Hadoop/Mapreduce in Python',
+      author='Diego Velasquez',
+      author_email='ciegovr@gmail.com',
+      packages=['cdvr'],
+     )
